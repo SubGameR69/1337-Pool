fce78cedc5c3ba8c944d428177ad056f981411bd
SubGameR69e <azjoker333@gmail.com>
2024-08-23 23:25:14 +0100

    ex06

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 20:19:37 by ssbaytri          #+#    #+#             */
+/*   Updated: 2024/08/15 16:28:11 by ssbaytri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnmbr(int n)
+{
+	ft_putchar('0' + n / 10);
+	ft_putchar('0' + n % 10);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putnmbr(a);
+			ft_putchar(' ');
+			ft_putnmbr(b);
+			if (a != 98 || b != 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+}
