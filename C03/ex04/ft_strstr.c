bc007d761122fb4a990ca8fde9c65db03d9e322d
SubGameR69e <azjoker333@gmail.com>
2024-08-30 19:57:15 +0100

    ex04

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 12:38:14 by ssbaytri          #+#    #+#             */
+/*   Updated: 2024/08/03 12:43:19 by ssbaytri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (!to_find[i])
+		return (str);
+	while (str[i])
+	{
+		j = 0;
+		while (str[i + j] == to_find[j] && to_find[j])
+		{
+			j++;
+			if (!to_find[j])
+			{
+				return (str + i);
+			}
+		}
+		i++;
+	}
+	return (0);
+}
