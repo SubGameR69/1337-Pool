04b19c4b593f5eae7446468f75cfa48e725e6ab1
SubGameR69e <azjoker333@gmail.com>
2024-08-30 19:55:01 +0100

    ex03

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 11:49:19 by ssbaytri          #+#    #+#             */
+/*   Updated: 2024/08/03 12:04:52 by ssbaytri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	int				j;
+
+	i = 0;
+	j = 0;
+	while (dest[j])
+	{
+		j++;
+	}
+	while (src[i] && i < nb)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
