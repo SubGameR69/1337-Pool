554e400d7a3cad95d0b88d639ad362fcb67cf943
SubGameR69e <azjoker333@gmail.com>
2024-09-01 00:44:40 +0100

    ex07

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 16:21:04 by ssbaytri          #+#    #+#             */
+/*   Updated: 2024/08/06 16:24:32 by ssbaytri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 1)
+		return (0);
+	if (nb == 2)
+		return (1);
+	if (nb % 2 == 0)
+		return (0);
+	i = 2;
+	while (i < nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (nb <= 2)
+		return (2);
+	if (nb % 2 == 0)
+		nb++;
+	while (!ft_is_prime(nb))
+	{
+		nb += 2;
+	}
+	return (nb);
+}
