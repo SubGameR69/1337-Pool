b2c7ceab8e2213ee4207da6efc300f0c397578da
SubGameR69e <azjoker333@gmail.com>
2024-09-01 00:33:27 +0100

    ex00

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 12:26:57 by ssbaytri          #+#    #+#             */
+/*   Updated: 2024/08/06 12:30:21 by ssbaytri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	result = 1;
+	if (nb < 0)
+		return (0);
+	while (nb > 1)
+	{
+		result *= nb;
+		nb--;
+	}
+	return (result);
+}
