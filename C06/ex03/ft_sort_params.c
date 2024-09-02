667f67dd20a26abdd54e42c2a103884d78a4ba56
SubGameR69e <azjoker333@gmail.com>
2024-09-02 22:08:00 +0100

    ex03

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 16:31:10 by ssbaytri          #+#    #+#             */
+/*   Updated: 2024/08/08 16:46:19 by ssbaytri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && (s1[i] || s2[i]))
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	swap_str(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		j = i + 1;
+		while (j < ac)
+		{
+			if (ft_strcmp(av[i], av[j]) > 0)
+				swap_str(&av[i], &av[j]);
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < ac)
+	{
+		ft_putstr(av[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
