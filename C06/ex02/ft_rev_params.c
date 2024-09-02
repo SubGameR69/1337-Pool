05d795976b82966700ae4b8f94dbc772a8658a34
SubGameR69e <azjoker333@gmail.com>
2024-09-02 22:06:20 +0100

    ex02

---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 16:23:22 by ssbaytri          #+#    #+#             */
+/*   Updated: 2024/08/08 16:24:08 by ssbaytri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = ac - 1;
+	while (i > 0)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			ft_putchar(av[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i--;
+	}
+	return (0);
+}
