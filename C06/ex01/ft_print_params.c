46fadc655bb144da3e3fb0b2a97306e29b9a81ea
SubGameR69e <azjoker333@gmail.com>
2024-09-02 22:05:45 +0100

    ex01

---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 16:16:30 by ssbaytri          #+#    #+#             */
+/*   Updated: 2024/08/08 16:20:09 by ssbaytri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			ft_putchar(av[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
