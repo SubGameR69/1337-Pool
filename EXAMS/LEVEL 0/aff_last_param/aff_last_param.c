f59cb8d81b550f53ff96fa8f20951ac053bf4435
SubGameR69e <azjoker333@gmail.com>
2024-09-09 19:32:20 +0100

    1337

---
+++ EXAMS/LEVEL 0/aff_last_param/aff_last_param.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc > 1)
+	{
+		while (argv[argc - 1][i])
+		{
+			write(1, &argv[argc - 1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
