c4091adfdcaab3c3c01bb05181be17d51efd166a
SubGameR69e <azjoker333@gmail.com>
2024-09-15 13:56:09 +0100

    1337

---
+++ EXAMS/LEVEL 1/rot_13/rot_13.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+  if (ac == 2)
+  {
+    int i;
+
+    i = 0;
+    while (av[1][i])
+    {
+      if (av[1][i] >= 'a' && av[1][i] <= 'z')
+        av[1][i] = (av[1][i] - 'a' + 13) % 26 + 'a';
+      else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+          av[1][i] = (av[1][i] - 'A' + 13) % 26 + 'A';
+      write(1, &av[1][i], 1);
+      i++;
+    }
+  }
+  write(1, "\n", 1);
+  return (0);
+}
