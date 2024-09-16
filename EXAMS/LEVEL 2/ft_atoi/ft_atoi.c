79a16d38d7e354a750e7938b6eaab68397acefb9
SubGameR69e <azjoker333@gmail.com>
2024-09-16 22:42:40 +0100

    1337

---
+++ EXAMS/LEVEL 2/ft_atoi/ft_atoi.c
@@ -0,0 +1,29 @@
+int	ft_atoi(const char *str)
+{
+	int i;
+	int result;
+	int sign;
+
+	i = 0;
+	result = 0;
+	sign = 1; 
+
+
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+	{
+		i++;
+	}
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
