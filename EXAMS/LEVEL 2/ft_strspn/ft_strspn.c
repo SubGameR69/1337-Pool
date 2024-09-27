8882dc9b72dfb2a783b4ec9885af535a73876d4e
SubGameR69e <azjoker333@gmail.com>
2024-09-27 22:42:11 +0100

    1337

---
+++ EXAMS/LEVEL 2/ft_strspn/ft_strspn.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+size_t			ft_strspn(const char *s, const char *accept)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+				break ;
+			j++;
+		}
+		if (!accept[j])
+			break ;
+		i++;
+	}
+	return (i);
+}
